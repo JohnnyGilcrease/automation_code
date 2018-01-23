70e6888f9af6389fd6c3e6b54f49712316d2587d
Johnny Gilcrease <john@twinenginelabs.com>
2018-01-23 15:24:08 -0600

    intern

--- intern/deployed_version.rb
+++ intern/deployed_version.rb
@@ -42,7 +42,7 @@ HARVEST_CLIENT = Harvest.hardy_client(
     password: "568020jg"
   }
 )
-
+binding.pry
 def create_client(payload)
   client_name = payload["acceptanceData"]["name"] rescue nil
   if client_name
@@ -70,15 +70,59 @@ def create_project(payload)
     ) rescue nil
     new_project = HARVEST_CLIENT.projects.all.select { |item| item.name == project_name }[0]
     payload["new_project"] = new_project
+    payload["configured_quote"] = configured_quote
   end
   create_task_assignments(payload)
 end
 
 def create_task_assignments(payload)
-  binding.pry
+  task1 = payload["configured_quote"]["items"][0]["rate"]["name"] rescue nil
+  task2 = payload["configured_quote"]["ui"]["items"][0]["rate"]["name"] rescue nil
+  if task1
+    HARVEST_CLIENT.tasks.create(
+      Harvest::Task.new(
+        { name: task1, 
+          hourly_rate: payload["configured_quote"]["items"][0]["rate"]["rate"], 
+          billable: true
+        }
+      )
+    ) rescue nil
+    new_task1 = HARVEST_CLIENT.tasks.all.select{ |item| item.name == task1 }[0]
+    payload["new_task1"] = new_task1
+    task_assignment1 = HARVEST_CLIENT.task_assignments.create(
+      Harvest::TaskAssignment.new(
+        {
+          task_id: payload['new_task1']['id'], 
+          project_id: payload['new_project']['id']
+        }
+      )
+    ) rescue nil
+    if task2
+      HARVEST_CLIENT.tasks.create(
+      Harvest::Task.new(
+        { name: task2, 
+          hourly_rate: payload["configured_quote"]["ui"]["items"][0]["rate"]["rate"], 
+          billable: true
+        }
+      )
+    ) rescue nil
+    new_task2 = HARVEST_CLIENT.tasks.all.select{ |item| item.name == task2 }[0]
+    payload["new_task2"] = new_task2
+    task_assignment2 = HARVEST_CLIENT.task_assignments.create(
+      Harvest::TaskAssignment.new(
+        {
+          task_id: payload['new_task2']['id'], 
+          project_id: payload['new_project']['id']
+        }
+      )
+    ) rescue nil
+    end
+  end
 end
 
 def create_user_assignments(payload)
+  user_assignment1 = Harvest::UserAssignment.new(user_id: user.id, project_id: project.id)
+  harvest.user_assignments.create(user_assignment)
 end
 
 create_client(payload)
--- intern/newteam.rb
+++ intern/newteam.rb
@@ -7,23 +7,21 @@ password = 'harvest2017'
 
 harvest = Harvest.hardy_client(subdomain: subdomain, username: username, password: password)
 
-user = Harvest::User.new(first_name: 'Jane1', last_name: 'Doe1', email: 'jane1@doe.com', timezone: :est, password: 'secure')
-user = harvest.users.create(user)
 
+user = Harvest::User.new(first_name: '', last_name: '', email: '', timezone: :est, password: '')
+user = harvest.users.create(user)
 user_assignment = Harvest::UserAssignment.new(user_id: user.id, project_id: project.id)
 harvest.user_assignments.create(user_assignment)
-puts 'Assigned Jane1 Doe1 to the project'
 
-user = Harvest::User.new(first_name: 'Jane2', last_name: 'Doe2', email: 'jane2@doe.com', timezone: :est, password: 'secure')
+
+
+user = Harvest::User.new(first_name: '', last_name: '', email: '', timezone: :est, password: '')
 user = harvest.users.create(user)
+user_assignment = Harvest::UserAssignment.new(user_id: user.id, project_id: project.id)
 
+
+
+user = Harvest::User.new(first_name: '', last_name: '', email: '', timezone: :est, password: '')
+user = harvest.users.create(user)
 user_assignment = Harvest::UserAssignment.new(user_id: user.id, project_id: project.id)
 harvest.user_assignments.create(user_assignment)
-puts 'Assigned Jane2 Doe2 to the project'
-
-user = Harvest::User.new(first_name: 'Jane3', last_name: 'Doe3', email: 'jane3@doe.com', timezone: :est, password: 'secure')
-user = harvest.users.create(user)
-
-user_assignment = Harvest::UserAssignment.new(user_id: user.id, project_id: project.id)
-harvest.user_assignments.create(user_assignment)
-puts 'Assigned Jane3 Doe3 to the project'
