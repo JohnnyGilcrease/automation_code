3d08a691dc85ef05e4f299c22f51017f4b438259
Johnny Gilcrease <john@twinenginelabs.com>
2018-01-26 11:50:23 -0600

    intern

---
+++ Slack API/slack_channel_user.rb
@@ -0,0 +1,14 @@
+require 'slack-ruby-client'
+
+Slack.configure do |config|
+  config.token = ENV['SLACK_API_TOKEN']
+  raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+end
+
+client = Slack::Web::Client.new
+
+client.auth_test
+
+channels.create (name:)
+
+users.invite (name:)
